90a878f16919f4d4193a2feaccdad517ad04ec16
bharghav sagar <sagarbharghav2@gmail.com>
2023-07-28 17:16:52 -0400

    Api fixes

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
 const routes = require('./routes');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
@@ -19,12 +21,19 @@ app.use('/medclinic', require('../server/routes/doctorRoutes.js'));
 app.use('/medclinic', require('../server/routes/videoRoutes.js'));
 app.use('/medclinic', require('../server/routes/prescriptionRoutes.js'));
 app.use('/medclinic', require('../server/routes/paymentsRoutes.js'));
-app.use('/medclinic', require('../server/routes/notificationRoutes.js'));
+app.use('/medclinic', require('../server/routes/notificationRoutes'));
 
-// Route for root URL
-app.get('/', (req, res) => {
-  res.send('Welcome to the MedClinic backend API!');
+
+// Serve static files in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  });
+}
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
-
-// Export the Express app for use as a serverless function
-module.exports = app;
